a30a191afc99b542b27c707be4d1e5000eadc0c9
fartsucker9000 <ethanrhoward2013@gmail.com>
2024-12-16 16:20:01 -0500

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
-var sitename = "native."; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var sitename = "Waffle's Unblocked Games"; // Change this to change the name of your website.
+var subtext = "Waffle's Unblocked Games, The best unblocked site for school!"; // set the subtext
 
 // more settings in main.css
 
